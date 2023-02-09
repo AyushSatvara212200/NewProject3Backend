dfe53eedb33bcbab47744c8e32328116fe7909ae
Ankit Bhatt <Ankitbhatt511@gmail.com>
2023-02-09 23:19:58 +0530

    - added api

--- index.js
+++ index.js
@@ -1,97 +1,84 @@
-const express = require('express');
-const port = 9000;
-const mongoose = require('mongoose');
+require("dotenv").config();
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+
+const mongoose = require("mongoose");
+const cors = require("cors");
+// const bodyParser = require('body-parser');
+// const pdf = require('html-pdf');
+const passport = require("passport");
+
+//=== 1 - CREATE APP
+// Creating express app and configuring middleware needed for authentication
 const app = express();
-const cors = require('cors');
-const userModel = require('./models/user.model');
-const nomineeModel = require('./models/nominee.detail')
+
+//=== 3 - INITIALIZE PASSPORT MIDDLEWARE
+app.use(passport.initialize());
+
+// view engine setup
+app.set("views", path.join(__dirname, "src/views"));
+app.set("view engine", "jade");
+
+app.use(logger("dev"));
+app.disable("etag");
+
+// Setting up port
+const connUri = process.env.MONGO_LOCAL_CONN_URL;
+let PORT = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
 
-mongoose.connect("mongodb+srv://admin:admin@cluster0.lptfhbe.mongodb.net/CH");
+console.log("here");
 
-app.post("/signup", (req, res) => {
-    const { username, phone, email, password } = req.body;
+//=== 2 - SET UP DATABASE
+//Configure mongoose's promise to global promise
+mongoose.promise = global.Promise;
+mongoose.connect(connUri, { useNewUrlParser: true });
+// useCreateIndex: true
 
-    userModel.findOne({ phone: phone }, (err, found) => {
-        if (found) {
-            res.send({ message: "User already exists !!!" })
-        } else {
-            const userNew = new userModel({
-                username,
-                phone,
-                email,
-                password
-            })
-            userNew.save((err) => {
-                if (err) {
-                    res.send(err)
-                } else {
-                    res.send({ message: "User Successfully Created" })
-                }
-            })
-        }
-    })
-
-})
-
-app.post("/nomineeDetail", (req, res) => {
-    const { nomineeName,
-        nomineeGender,
-        nomineeRelation,
-        nomineeEmail,
-        nomineePhone,
-        nomineeAddress } = req.body;
-    nomineeModel.findOne({ nomineePhone: nomineePhone }, (err, found) => {
-        if (found) {
-            res.send({ message: "Nominee already exists !!!" })
-        } else {
-            const nomineeNew = new nomineeModel({
-                nomineeName,
-                nomineeGender,
-                nomineeRelation,
-                nomineeEmail,
-                nomineePhone,
-                nomineeAddress
-            })
-            nomineeNew.save((err) => {
-                if (err) {
-                    res.send(err)
-                } else {
-                    res.send({ message: "Nominee Successfully Created" })
-                }
-            })
-        }
-    })
-})
-
-app.post("/login", (req, res) => {
-    const { phone, password } = req.body;
-    userModel.findOne({ phone: phone }, (err, found) => {
-        if (found) {
-            if (password === found.password) {
-                res.send({ message: "Logged in successfully", found: found });
-                // localStorage.setItem("token","abcdefghijklmnopqrstuvwxyz");
-            } else {
-                res.send({ message: "Incorrect Password" })
-            }
-        } else {
-            res.send({ message: "User not Exists" })
-        }
-    })
-})
-
-app.get("/read", async (req,res)=>{
-    nomineeModel.find({},(err,result)=>{
-        if (err) {
-            res.send(err)
-        }else{
-            res.send(result)
-        }
-    })
+const connection = mongoose.connection;
+connection.once("open", () =>
+  console.log("MongoDB --  database connection established successfully!")
+);
+connection.on("error", (err) => {
+  console.log(
+    "MongoDB connection error. Please make sure MongoDB is running. " + err
+  );
+  process.exit();
 });
 
-app.listen(process.env.PORT || 9000, () => {
-    console.log(`You are connected to port ${port}`)
-})
+require("./src/middlewares/jwt")(passport);
+
+//=== 4 - CONFIGURE ROUTES
+//Configure Route
+require("./src/routes/index")(app);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+module.exports = app;
+
+//=== 5 - START SERVER
+app.listen(PORT, () =>
+  console.log("Server listening on http://localhost:" + PORT + "/")
+);
